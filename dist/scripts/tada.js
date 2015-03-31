b5c9287bc7689f4df9954e15b73e6bd61bf0cfcb
Ofir Dagan <ofirdagan@gmail.com>
2015-03-31 14:09:29 +0300

    update dist

--- dist/scripts/tada.js
+++ dist/scripts/tada.js
@@ -1,53 +1,80 @@
 "use strict";
 
-angular.module("tada", []).service("tadaUtils", [ "$q", "$rootScope", function($q, $rootScope) {
-    function serializeArgs(args) {
-        return JSON.stringify(Array.prototype.slice.call(args));
-    }
-    function createAsyncFunc(name) {
-        return (function () {
+angular.module("tada", []).provider("tadaUtils", [ "$provide", function($provide) {
+    this.mock = function() {
+        var args = Array.prototype.slice.call(arguments);
+        args.forEach(function(service) {
+            $provide.service(service, [ "$injector", function($injector) {
+                return $injector.get(service + "Mock");
+            } ]);
+        });
+    };
+    this.$get = [ "$q", "$rootScope", function($q, $rootScope) {
+        function serializeArgs(args) {
+            return JSON.stringify(Array.prototype.slice.call(args));
+        }
+        function createAsyncFunc(name) {
             var defer = $q.defer();
             var firstCall = true;
             var calledWithArgs;
-            var func = jasmine.createSpy(name).andCallFake(function () {
+            var func = jasmine.createSpy(name).andCallFake(function() {
                 calledWithArgs = serializeArgs(arguments);
                 defer = firstCall ? defer : $q.defer();
                 firstCall = false;
                 return defer.promise;
             });
-            func.returns = function (value) {
+            func.returns = function(value) {
                 defer.resolve(value);
                 $rootScope.$digest();
             };
-            func.whenCalledWithArgs = function () {
+            func.whenCalledWithArgs = function() {
                 var expectedCalledArgs = serializeArgs(arguments);
                 return {
-                    returns: function (value) {
+                    returns: function(value) {
                         if (expectedCalledArgs === calledWithArgs) {
                             defer.resolve(value);
                             $rootScope.$digest();
                         }
                     },
-                    rejects: function (value) {
+                    rejects: function(value) {
                         defer.reject(value);
                         $rootScope.$digest();
                     }
                 };
             };
-            func.rejects = function (value) {
+            func.rejects = function(value) {
                 defer.reject(value);
                 $rootScope.$digest();
             };
             return func;
-        })();
-    }
-    function createSyncFunc(name) {
-        var func = jasmine.createSpy(name);
-        func.returns = function(value) {
-            func.andReturn(value);
+        }
+        function createFunc(name) {
+            var calledWithArgs;
+            var func = jasmine.createSpy(name).andCallFake(function() {
+                calledWithArgs = serializeArgs(arguments);
+            });
+            func.returns = function(value) {
+                func.andReturn(value);
+            };
+            func.whenCalledWithArgs = function() {
+                var expectedCalledArgs = serializeArgs(arguments);
+                return {
+                    returns: function(value) {
+                        return func.andCallFake(function() {
+                            if (serializeArgs(arguments) === expectedCalledArgs) {
+                                return value;
+                            }
+                        });
+                    }
+                };
+            };
+            return func;
+        }
+        return {
+            aSyncedFunc: createFunc,
+            anAsyncFunc: createAsyncFunc,
+            createFunc: createFunc,
+            createAsyncFunc: createAsyncFunc
         };
-        return func;
-    }
-    this.aSyncedFunc = createSyncFunc;
-    this.anAsyncFunc = createAsyncFunc;
+    } ];
 } ]);
